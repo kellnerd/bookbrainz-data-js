352a345953d999431ef3b6ece85a022ce77779bd
David Kellner <52860029+kellnerd@users.noreply.github.com>
2023-03-17 19:13:09 +0100

    chore: restore copyright lines of the previous Bookshelf JS models
    Although there is no original code left, I do not want these modules to
    look like I had defined all the models from scratch.

--- src/models/identifier.ts
+++ src/models/identifier.ts
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2023  David Kellner
+ * Copyright (C) 2016  Ben Ockmore
+ *               2023  David Kellner
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
